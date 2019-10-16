29a2f8452b12445670580b2d54ad9bc5a635b666
Andrei Kostyrin <kostyrin2@gmail.com>
2019-10-16 10:36:19 +0200

    StoreDynamicPropertyObjectValue migration fix

--- src/VirtoCommerce.StoreModule.Data/Migrations/20190628091147_AddStoreDynamicPropertyObjectValue.cs
+++ src/VirtoCommerce.StoreModule.Data/Migrations/20190628091147_AddStoreDynamicPropertyObjectValue.cs
@@ -38,7 +38,7 @@ namespace VirtoCommerce.StoreModule.Data.Migrations
                         column: x => x.ObjectId,
                         principalTable: "Store",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
+                        onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateIndex(
