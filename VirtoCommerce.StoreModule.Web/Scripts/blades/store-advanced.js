a1ec4ceebd26bfba60e430753f4b5f019f3ae219
ilyawzrd <ilya.ivanov6@gmail.com>
2020-01-09 17:50:55 +0200

    fix pr comments

--- VirtoCommerce.StoreModule.Web/Scripts/blades/store-advanced.js
+++ VirtoCommerce.StoreModule.Web/Scripts/blades/store-advanced.js
@@ -17,7 +17,7 @@ angular.module('virtoCommerce.storeModule')
             $scope.bladeClose();
         }
 
-        $scope.blade.refresh = function () {            
+        $scope.blade.refresh = function () {
             initialize();
         }
 
@@ -28,51 +28,63 @@ angular.module('virtoCommerce.storeModule')
                 template: 'Modules/$(VirtoCommerce.Inventory)/Scripts/blades/fulfillment-center-list.tpl.html'
             };
             bladeNavigationService.showBlade(newBlade, $scope.blade);
-        }        
+        }
 
         $scope.blade.headIcon = 'fa-archive';
-        $scope.pageSize = 50;                
+        // pageSize amount must be enough to show scrollbar in dropdown list counter.
+        // If scrollbar doesn't appear then auto loading won't work.
+        $scope.pageSize = 50;
 
         $scope.blade.isLoading = false;
         $scope.blade.currentEntity = angular.copy($scope.blade.entity);
         $scope.blade.origEntity = $scope.blade.entity;
         $scope.countries = countries.query();
         $scope.timeZones = timeZones.query();
-        
+
         function initialize() {
             $scope.fulfillmentCenters = [];
-            $scope.fulfillmentsIds = [];            
+            $scope.fulfillmentsIds = [];
             loadFulfillmentCenters();
             fulfillments.search({ take: $scope.pageSize }, function (data) {
-                joinFulfillmentCenters(data.results);                
+                // Redefine pageSize based on totalCount of fulfillment centers count so scroll will work correctly
+                $scope.pageSize = Math.round(data.totalCount * 0.65);
+                joinFulfillmentCenters(data.results);
             });
         }
 
-        $scope.refreshFulfillmentCenters = function ($select) {                      
+        $scope.refreshFulfillmentCenters = function ($select) {
+            $scope.fulfillmentCenters = [];
+            $scope.fulfillmentsIds = [];
+            loadFulfillmentCenters();
             $scope.fetchNextFulfillmentCenters($select);
         }
 
-        $scope.fetchNextFulfillmentCenters = function ($select) {            
+        $scope.fetchNextFulfillmentCenters = function ($select) {
             fulfillments.search({ searchPhrase: $select.search, take: $scope.pageSize, skip: $scope.fulfillmentCenters.length }, function (data) {
                 joinFulfillmentCenters(data.results);
-                console.log($scope.fulfillmentCenters.length);                
-            }); 
-        }        
+                console.log($scope.fulfillmentCenters.length);
+            });
+        }
 
-        function loadFulfillmentCenters() {                        
-            $scope.fulfillmentsIds = _.uniq($scope.fulfillmentsIds.concat($scope.blade.currentEntity.mainFulfillmentCenterId, $scope.blade.currentEntity.mainReturnsFulfillmentCenterId, $scope.blade.currentEntity.additionalFulfillmentCenterIds, $scope.blade.currentEntity.returnsFulfillmentCenterIds));
+        function loadFulfillmentCenters() {
+            var mainFulfillmentCenterId = $scope.blade.currentEntity.mainFulfillmentCenterId;
+            var mainReturnsFulfillmentCenterId = $scope.blade.currentEntity.mainReturnsFulfillmentCenterId;
+            var additionalFulfillmentCenterIds = $scope.blade.currentEntity.additionalFulfillmentCenterIds;
+            var returnsFulfillmentCenterIds = $scope.blade.currentEntity.returnsFulfillmentCenterIds;
+            $scope.fulfillmentsIds = $scope.fulfillmentsIds.concat(mainFulfillmentCenterId, mainReturnsFulfillmentCenterId, additionalFulfillmentCenterIds, returnsFulfillmentCenterIds);
+            $scope.fulfillmentsIds = _.uniq($scope.fulfillmentsIds);
             $scope.fulfillmentsIds = _.filter($scope.fulfillmentsIds, function (value) {
                 return value != undefined;
             });
             if ($scope.fulfillmentsIds.length > 0) {
                 fulfillments.getByIds($scope.fulfillmentsIds, function (data) {
                     joinFulfillmentCenters(data);
-                });                 
-            }            
+                });
+            }
         }
 
-        function joinFulfillmentCenters(centers) {            
-            $scope.fulfillmentCenters = _.uniq($scope.fulfillmentCenters.concat(centers), 'id');                                       
+        function joinFulfillmentCenters(centers) {
+            $scope.fulfillmentCenters = _.uniq($scope.fulfillmentCenters.concat(centers), 'id');
         }
 
         initialize();
