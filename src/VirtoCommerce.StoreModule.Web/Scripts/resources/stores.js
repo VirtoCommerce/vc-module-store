be4ec8fe0cb8784dbf46bb3bb3fa18eabdac29c9
Andrei Kostyrin <kostyrin2@gmail.com>
2020-03-10 16:05:18 +0200

    #vp-1466 loginOnBehalf move to the platform

--- src/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
+++ src/VirtoCommerce.StoreModule.Web/Scripts/resources/stores.js
@@ -3,7 +3,6 @@
     return $resource('api/stores', {}, {
         search: { method: 'POST', url: 'api/stores/search' },
         get: { url: 'api/stores/:id' },
-        update: { method: 'PUT' },
-        queryLoginOnBehalfStores: { url: 'api/stores/allowed/:userId', isArray: true }
+        update: { method: 'PUT' }
     });
 }]);
