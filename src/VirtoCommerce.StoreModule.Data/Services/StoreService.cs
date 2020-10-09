f546b5f6def96b11611da13dc1347ef4b737187f
Andrey Kozlov <yecli.github@gmail.com>
2020-10-09 11:01:19 +0500

    VP-5479: Fix store import error

--- src/VirtoCommerce.StoreModule.Data/Services/StoreService.cs
+++ src/VirtoCommerce.StoreModule.Data/Services/StoreService.cs
@@ -90,6 +90,11 @@ namespace VirtoCommerce.StoreModule.Data.Services
 
                     if (targetEntity != null)
                     {
+                        /// This extension is allow to get around breaking changes is introduced in EF Core 3.0 that leads to throw
+                        /// Database operation expected to affect 1 row(s) but actually affected 0 row(s) exception when trying to add the new children entities with manually set keys
+                        /// https://docs.microsoft.com/en-us/ef/core/what-is-new/ef-core-3.0/breaking-changes#detectchanges-honors-store-generated-key-values
+                        repository.TrackModifiedAsAddedForNewChildEntities(targetEntity);
+
                         changedEntries.Add(new GenericChangedEntry<Store>(store, targetEntity.ToModel(AbstractTypeFactory<Store>.TryCreateInstance()),
                             EntryState.Modified));
                         sourceEntity.Patch(targetEntity);
