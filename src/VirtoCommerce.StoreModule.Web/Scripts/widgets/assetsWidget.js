6fa3fabdaba9353d0f058306a0837829a87ceadc
Andrei Kolchanov <akak1977@gmail.com>
2021-11-16 19:17:48 +0500

    fix: Use assets from asset module

--- src/VirtoCommerce.StoreModule.Web/Scripts/widgets/assetsWidget.js
+++ src/VirtoCommerce.StoreModule.Web/Scripts/widgets/assetsWidget.js
@@ -6,8 +6,8 @@
         var newBlade = {
             id: "storeAssetList",
             subtitle: blade.title,
-            controller: 'platformWebApp.assets.assetListController',
-            template: '$(Platform)/Scripts/app/assets/blades/asset-list.tpl.html',
+            controller: 'virtoCommerce.assetsModule.assetListController',
+            template: 'Modules/$(VirtoCommerce.Assets)/Scripts/blades/asset-list.tpl.html',
             currentEntity: { url: '/stores/' + blade.currentEntityId }
         };
 
