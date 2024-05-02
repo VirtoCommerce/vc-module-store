8b6bd0782d10fb2a4146f45655bfae152ec16a46
Oleg Zhuk <zhukoo@gmail.com>
2024-05-02 10:54:29 +0200

    Added Obsolete for StoreSearchResult.Stores

--- src/VirtoCommerce.StoreModule.Core/Model/Search/StoreSearchResult.cs
+++ src/VirtoCommerce.StoreModule.Core/Model/Search/StoreSearchResult.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using VirtoCommerce.Platform.Core.Common;
 
@@ -5,6 +6,7 @@ namespace VirtoCommerce.StoreModule.Core.Model.Search
 {
     public class StoreSearchResult : GenericSearchResult<Store>
     {
+        [Obsolete("Use Results instead of Stores", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
         public IList<Store> Stores => Results;
     }
 }
