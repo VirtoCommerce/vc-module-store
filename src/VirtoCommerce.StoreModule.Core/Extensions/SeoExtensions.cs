57603e4c5ad2b486ed0cebba5d7842f2464bea32
Basil Kotov <basilkot@gmail.com>
2025-06-02 12:00:29 +0100

    remove slug and permalink from score seo infos

--- src/VirtoCommerce.StoreModule.Core/Extensions/SeoExtensions.cs
+++ src/VirtoCommerce.StoreModule.Core/Extensions/SeoExtensions.cs
@@ -10,17 +10,17 @@ public static class SeoExtensions
     /// <summary>
     /// Returns SEO record with the highest score
     /// </summary>
-    public static SeoInfo GetBestMatchingSeoInfo(this ISeoSupport seoSupport, Store store, string language, string slug = null, string permalink = null)
+    public static SeoInfo GetBestMatchingSeoInfo(this ISeoSupport seoSupport, Store store, string language)
     {
-        return seoSupport?.SeoInfos?.GetBestMatchingSeoInfo(store, language, slug, permalink);
+        return seoSupport?.SeoInfos?.GetBestMatchingSeoInfo(store, language);
     }
 
     /// <summary>
     /// Returns SEO record with the highest score
     /// </summary>
-    public static SeoInfo GetBestMatchingSeoInfo(this IEnumerable<SeoInfo> seoInfos, Store store, string language, string slug = null, string permalink = null)
+    public static SeoInfo GetBestMatchingSeoInfo(this IEnumerable<SeoInfo> seoInfos, Store store, string language)
     {
-        return seoInfos?.GetBestMatchingSeoInfo(store?.Id, store?.DefaultLanguage, language ?? store?.DefaultLanguage, slug, permalink);
+        return seoInfos?.GetBestMatchingSeoInfo(store?.Id, store?.DefaultLanguage, language ?? store?.DefaultLanguage);
     }
 
 }
