970d4f3c3fd92cf91b759f536c648f87cd1a7e3e
yuskithedeveloper <yuskianywhere@gmail.com>
2025-11-18 13:11:24 -0600

    Localized settings handling, languages and currencies

--- src/VirtoCommerce.StoreModule.Data/Services/ComparableStoreSettingsProvider.cs
+++ src/VirtoCommerce.StoreModule.Data/Services/ComparableStoreSettingsProvider.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using VirtoCommerce.EnvironmentsCompare.Core.Models;
 using VirtoCommerce.EnvironmentsCompare.Core.Services;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.StoreModule.Core.Model.Search;
 using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.StoreModule.Data.Services;
 
-public class ComparableStoreSettingsProvider(IStoreSearchService storeSearchService) : IComparableSettingsProvider
+public class ComparableStoreSettingsProvider(IStoreSearchService storeSearchService, ILocalizableSettingService localizableSettingService) : IComparableSettingsProvider
 {
     public async Task<IList<ComparableSettingScope>> GetComparableSettingsAsync()
     {
@@ -35,14 +37,62 @@ public class ComparableStoreSettingsProvider(IStoreSearchService storeSearchServ
                 {
                     var resultSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
                     resultSetting.Name = storeSetting.Name;
-                    resultSetting.Value = storeSetting.Value;
+                    resultSetting.Value = await GetSettingValue(storeSetting, store);
                     resultSetting.IsSecret = IsSettingSecret(storeSetting);
                     resultGroup.Settings.Add(resultSetting);
                 }
             }
+
+            var languagesAndCurrenciesGroup = AbstractTypeFactory<ComparableSettingGroup>.TryCreateInstance();
+            languagesAndCurrenciesGroup.GroupName = "Languages and Currencies";
+            resultScope.SettingGroups.Add(languagesAndCurrenciesGroup);
+
+            var languagesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+            languagesSetting.Name = nameof(store.Languages);
+            languagesSetting.Value = string.Join(";", store.Languages.OrderBy(x => x));
+            languagesAndCurrenciesGroup.Settings.Add(languagesSetting);
+
+            var defaultLanguageSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+            defaultLanguageSetting.Name = nameof(store.DefaultLanguage);
+            defaultLanguageSetting.Value = store.DefaultLanguage;
+            languagesAndCurrenciesGroup.Settings.Add(defaultLanguageSetting);
+
+            var currenciesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+            currenciesSetting.Name = nameof(store.Currencies);
+            currenciesSetting.Value = string.Join(";", store.Currencies.OrderBy(x => x));
+            languagesAndCurrenciesGroup.Settings.Add(currenciesSetting);
+
+            var defaultCurrencySetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+            defaultCurrencySetting.Name = nameof(store.DefaultCurrency);
+            defaultCurrencySetting.Value = store.DefaultCurrency;
+            languagesAndCurrenciesGroup.Settings.Add(defaultCurrencySetting);
         }
 
         return result;
+    }
+
+    protected virtual async Task<object> GetSettingValue(ObjectSettingEntry setting, Store store)
+    {
+        if (!setting.IsLocalizable)
+        {
+            return setting.Value;
+        }
+        else
+        {
+            var localizedValuesBuilder = new StringBuilder();
+
+            foreach (var language in store.Languages.OrderBy(x => x))
+            {
+                localizedValuesBuilder.Append("[").Append(language).Append("]:");
+                foreach (var settingValue in await localizableSettingService.GetValuesAsync(setting.Name, language))
+                {
+                    localizedValuesBuilder.Append(settingValue.Key).Append("=").Append(settingValue.Value).Append(";");
+                }
+                localizedValuesBuilder.Append(";");
+            }
+
+            return localizedValuesBuilder.ToString();
+        }
     }
 
     protected virtual bool IsSettingSecret(ObjectSettingEntry setting)
