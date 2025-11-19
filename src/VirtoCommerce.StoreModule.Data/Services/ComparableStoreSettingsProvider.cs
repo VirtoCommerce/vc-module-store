b804871eb39c596a0638a126fc16484b3a65b57f
yuskithedeveloper <yuskianywhere@gmail.com>
2025-11-19 06:12:36 -0600

    Refactoring

--- src/VirtoCommerce.StoreModule.Data/Services/ComparableStoreSettingsProvider.cs
+++ src/VirtoCommerce.StoreModule.Data/Services/ComparableStoreSettingsProvider.cs
@@ -29,48 +29,31 @@ public class ComparableStoreSettingsProvider(IStoreSearchService storeSearchServ
             resultScope.ScopeName = $"StoreSettings: {store.Id}";
             result.Add(resultScope);
 
-            foreach (var storeSettingGroup in store.Settings.GroupBy(x => x.GroupName))
-            {
-                var resultGroup = AbstractTypeFactory<ComparableSettingGroup>.TryCreateInstance();
-                resultGroup.GroupName = storeSettingGroup.Key;
-                resultScope.SettingGroups.Add(resultGroup);
+            await AddSettings(resultScope, store);
 
-                foreach (var storeSetting in storeSettingGroup)
-                {
-                    var resultSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
-                    resultSetting.Name = storeSetting.Name;
-                    resultSetting.Value = await GetSettingValue(storeSetting, store);
-                    resultSetting.IsSecret = IsSettingSecret(storeSetting);
-                    resultGroup.Settings.Add(resultSetting);
-                }
-            }
-
-            var languagesAndCurrenciesGroup = AbstractTypeFactory<ComparableSettingGroup>.TryCreateInstance();
-            languagesAndCurrenciesGroup.GroupName = "Languages and Currencies";
-            resultScope.SettingGroups.Add(languagesAndCurrenciesGroup);
-
-            var languagesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
-            languagesSetting.Name = nameof(store.Languages);
-            languagesSetting.Value = string.Join(";", store.Languages.OrderBy(x => x));
-            languagesAndCurrenciesGroup.Settings.Add(languagesSetting);
-
-            var defaultLanguageSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
-            defaultLanguageSetting.Name = nameof(store.DefaultLanguage);
-            defaultLanguageSetting.Value = store.DefaultLanguage;
-            languagesAndCurrenciesGroup.Settings.Add(defaultLanguageSetting);
-
-            var currenciesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
-            currenciesSetting.Name = nameof(store.Currencies);
-            currenciesSetting.Value = string.Join(";", store.Currencies.OrderBy(x => x));
-            languagesAndCurrenciesGroup.Settings.Add(currenciesSetting);
-
-            var defaultCurrencySetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
-            defaultCurrencySetting.Name = nameof(store.DefaultCurrency);
-            defaultCurrencySetting.Value = store.DefaultCurrency;
-            languagesAndCurrenciesGroup.Settings.Add(defaultCurrencySetting);
+            await AddLanguagesAndCurrencies(resultScope, store);
         }
 
         return result;
+    }
+
+    protected virtual async Task AddSettings(ComparableSettingScope resultScope, Store store)
+    {
+        foreach (var storeSettingGroup in store.Settings.GroupBy(x => x.GroupName))
+        {
+            var resultGroup = AbstractTypeFactory<ComparableSettingGroup>.TryCreateInstance();
+            resultGroup.GroupName = storeSettingGroup.Key;
+            resultScope.SettingGroups.Add(resultGroup);
+
+            foreach (var storeSetting in storeSettingGroup)
+            {
+                var resultSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+                resultSetting.Name = storeSetting.Name;
+                resultSetting.Value = await GetSettingValue(storeSetting, store);
+                resultSetting.IsSecret = IsSettingSecret(storeSetting);
+                resultGroup.Settings.Add(resultSetting);
+            }
+        }
     }
 
     protected virtual async Task<object> GetSettingValue(ObjectSettingEntry setting, Store store)
@@ -100,5 +83,34 @@ public class ComparableStoreSettingsProvider(IStoreSearchService storeSearchServ
     protected virtual bool IsSettingSecret(ObjectSettingEntry setting)
     {
         return setting.ValueType == SettingValueType.SecureString;
+    }
+
+    protected virtual Task AddLanguagesAndCurrencies(ComparableSettingScope resultScope, Store store)
+    {
+        var languagesAndCurrenciesGroup = AbstractTypeFactory<ComparableSettingGroup>.TryCreateInstance();
+        languagesAndCurrenciesGroup.GroupName = "Languages and Currencies";
+        resultScope.SettingGroups.Add(languagesAndCurrenciesGroup);
+
+        var languagesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+        languagesSetting.Name = nameof(store.Languages);
+        languagesSetting.Value = string.Join(";", store.Languages.OrderBy(x => x));
+        languagesAndCurrenciesGroup.Settings.Add(languagesSetting);
+
+        var defaultLanguageSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+        defaultLanguageSetting.Name = nameof(store.DefaultLanguage);
+        defaultLanguageSetting.Value = store.DefaultLanguage;
+        languagesAndCurrenciesGroup.Settings.Add(defaultLanguageSetting);
+
+        var currenciesSetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+        currenciesSetting.Name = nameof(store.Currencies);
+        currenciesSetting.Value = string.Join(";", store.Currencies.OrderBy(x => x));
+        languagesAndCurrenciesGroup.Settings.Add(currenciesSetting);
+
+        var defaultCurrencySetting = AbstractTypeFactory<ComparableSetting>.TryCreateInstance();
+        defaultCurrencySetting.Name = nameof(store.DefaultCurrency);
+        defaultCurrencySetting.Value = store.DefaultCurrency;
+        languagesAndCurrenciesGroup.Settings.Add(defaultCurrencySetting);
+
+        return Task.CompletedTask;
     }
 }
